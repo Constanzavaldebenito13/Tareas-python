a8ef2744d164740a16f6603796e36d77be8eba4b
Constanzavaldebenito13 <co.valdebenitor@duocuc.cl>
2026-04-15 08:45:24 -0400

    Add files via upload

---
+++ clases.py
@@ -0,0 +1,15 @@
+print("Hola,mundo")
+v_enteras = 42 #variables entera
+v_decimales = 3.14 #variable decimales
+v_texto = "Hola"
+v_booleano = True
+
+Edad = int(input("Ingrese su edad"))
+
+print("Tu edad es de: ",Edad)
+
+Edad = int (input("Dime tu edad"))
+if Edad >=18:
+    print("Eres mayor: ")
+else:
+    print("Eres menor:")
